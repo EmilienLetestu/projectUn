25d140aab8d44d64184ac24a12c1787631ca9148
emil <eletestu@gmail.com>
2017-10-15 22:30:29 +0200

    [add] creete user class

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,12 +8,346 @@
 
 namespace App\Entity;
 
-
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
-
-class User
+/**
+ * Class User
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User implements AdvancedUserInterface, \Serializable
 {
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var
+     * @ORM\column(type="string", length =30)
+     */
+    private $name;
+
+    /**
+     * @var
+     * @ORM\column(type="string", length =30)
+     */
+    private $surname;
+
+    /**
+     * @var
+     * @ORM\column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var
+     * @ORM\column(type="string")
+     */
+    private $pswd;
+
+    /**
+     * @var
+     * @ORM\Column(type="string", length=15)
+     */
+    private $role;
+
+    /**
+     * @var
+     *  @ORM\column(type="date")
+     */
+    private $registeredOn;
+
+    /**
+     * @var
+     * @ORM\column(type="boolean", options={"default"=false})
+     */
+    private $activated = false;
+
+    /**
+     * @var bool
+     * @ORM\column(type="boolean", options={"default"=false})
+     */
+    private $deactivated =false;
+
+    /**
+     * @var
+     * @ORM\OneToMany(targetEntity="App\Entity\Story", mappedBy="user")
+     */
+    private $stories;
+
+    /**---------------------- setters & getters ------------------------*/
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $pswd
+     */
+    public function setPswd($pswd)
+    {
+        $this->pswd = $pswd;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getPswd()
+    {
+        return $this->pswd;
+    }
+
+    /**
+     * @param mixed $role
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+
+    /**
+     * @param mixed $registeredOn
+     */
+    public function setRegisteredOn($registeredOn)
+    {
+        $this->registeredOn = $registeredOn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRegisteredOn()
+    {
+        return $this->registeredOn;
+    }
+
+    /**
+     * @param mixed $activated
+     */
+    public function setActivated($activated)
+    {
+        $this->activated = $activated;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActivated()
+    {
+        return $this->activated;
+    }
+
+    /**
+     * @param bool $deactivated
+     */
+    public function setDeactivated($deactivated)
+    {
+        $this->deactivated = $deactivated;
+    }
+
+    public function getDeactivated()
+    {
+        return $this->deactivated;
+    }
+
+    /**---------------------- entity relation management------------------------*/
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        $this->stories = new ArrayCollection();
+    }
+
+    /**
+     * @param Story $story
+     */
+    public function addStory(Story $story)
+    {
+        $this->stories[] = $story;
+    }
+
+    /**
+     * @param Story $story
+     */
+    public function removeStory(Story $story)
+    {
+        $this->stories->removeElement($story);
+    }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getStories()
+    {
+       return $this->stories;
+    }
+
+    /**---------------------- advanceUserInterface methods------------------------*/
+
+    public function getUsername()
+    {
+        return $this->getEmail();
+    }
+
+    public function getPassword()
+    {
+        return $this->getPswd();
+    }
+
+    public function getRoles()
+    {
+        return $this->getRole();
+    }
+
+    /**
+     * default method from advanceUserInterface,
+     * must be declare even blank
+     */
+    public function eraseCredentials()
+    {
+
+    }
+
+    /**
+     * return null as pswd use bcrypt algorithm
+     * @return null
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function isAccountNonLocked()
+    {
+
+        return $this->getDeactivated();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function isEnabled()
+    {
+       return $this->getActivated();
+    }
+
+    /**
+     * @return string
+     */
+    public function serialize()
+    {
+        return serialize([
+            $this->getId(),
+            $this->getName(),
+            $this->getSurname(),
+            $this->getEmail()
+        ]);
+    }
+
+    /**
+     * @param string $serialized
+     */
+    public function unserialize($serialized)
+    {
+        list(
+            $this->id,
+            $this->name,
+            $this->surname,
+            $this->email
+            ) = $this->unserialize($serialized)
+        ;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
 
 }
