5936723fde2e38778f6ed16446718f9557ebd3f0
emilien_letestu <eletestu@gmail.com>
2017-10-26 13:24:42 +0200

    [add] more front validation rules

--- public/js/main.js
+++ public/js/main.js
@@ -12,6 +12,8 @@ $(document).ready(function () {
         var surname = $('#register_surname');
         var pswd    = $('#register_pswd');
         var confirm = $('#register_confirmPswd');
+        var email   = $('#register_email');
+        var regex   =  /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
         if(name.val().length < 3)
         {
@@ -59,11 +61,18 @@ $(document).ready(function () {
             $('#invalid_pswd_icon').hide();
             $('#valid_pswd_icon').show();
         }
-        if(confirm !== pswd)
+        if(confirm.val() !== pswd.val())
         {
             confirm.css('border-color','#F54041');
             $('#invalid_confirm').show();
             $('#invalid_confirm_icon').show();
+            $('#valid_confirm_icon').hide();
+            validate = false;
+        }
+        else if(pswd.val() == "")
+        {
+            $('#invalid_confirm').hide();
+            $('#invalid_confirm_icon').hide();
             $('#valid_confirm_icon').hide();
             validate = false;
         }
@@ -73,6 +82,45 @@ $(document).ready(function () {
             $('#invalid_confirm').hide();
             $('#invalid_confirm_icon').hide();
             $('#valid_confirm_icon').show();
+        }
+        if(!regex.test(email.val()))
+        {
+            email.css('border-color','#F54041');
+            $('#invalid_email').show();
+            $('#invalid_email_icon').show();
+            $('#valid_email_icon').hide();
+            validate = false;
+        }
+        else
+        {
+            email.css('border-color','#5fdda1');
+            $('#invalid_email').hide();
+            $('#invalid_email_icon').hide();
+            $('#valid_email_icon').show();
+        }
+        return validate;
+
+    });
+
+    $('#lostPswdBtn').click(function () {
+        validate = true;
+        var email   = $('#ask_new_pswd_email');
+        var regex   =  /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+
+        if(!regex.test(email.val()))
+        {
+            email.css('border-color','#F54041');
+            $('#invalid_email').show();
+            $('#invalid_email_icon').show();
+            $('#valid_email_icon').hide();
+            validate = false;
+        }
+        else
+        {
+            email.css('border-color','#5fdda1');
+            $('#invalid_email').hide();
+            $('#invalid_email_icon').hide();
+            $('#valid_email_icon').show();
         }
         return validate;
 
