3548740b6fea41817ee9210c3872de19a58d151b
emilien_letestu <eletestu@gmail.com>
2017-10-26 15:52:51 +0200

    [add] connection forms front validation part 2

--- public/js/main.js
+++ public/js/main.js
@@ -103,7 +103,7 @@ $(document).ready(function () {
     });
 
     $('#lostPswdBtn').click(function () {
-        validate = true;
+        var validate = true;
         var email   = $('#ask_new_pswd_email');
         var regex   =  /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
 
@@ -124,6 +124,52 @@ $(document).ready(function () {
         }
         return validate;
 
+    });
+
+    $('#newPswdBtn').click(function (){
+
+        var validate = true;
+        var pswd     = $('#new_pswd_pswd');
+        var confirm  = $('#new_pswd_confirmPswd');
+
+        if(pswd.val().length < 6)
+        {
+            pswd.css('border-color','#F54041');
+            $('#invalid_pswd').show();
+            $('#invalid_pswd_icon').show();
+            $('#valid_pswd_icon').hide();
+            validate = false;
+        }
+        else
+        {
+            pswd.css('border-color','#5fdda1');
+            $('#invalid_pswd').hide();
+            $('#invalid_pswd_icon').hide();
+            $('#valid_pswd_icon').show();
+        }
+        if(confirm.val() !== pswd.val())
+        {
+            confirm.css('border-color','#F54041');
+            $('#invalid_confirm').show();
+            $('#invalid_confirm_icon').show();
+            $('#valid_confirm_icon').hide();
+            validate = false;
+        }
+        else if(pswd.val() == "")
+        {
+            $('#invalid_confirm').hide();
+            $('#invalid_confirm_icon').hide();
+            $('#valid_confirm_icon').hide();
+            validate = false;
+        }
+        else
+        {
+            confirm.css('border-color','#5fdda1');
+            $('#invalid_confirm').hide();
+            $('#invalid_confirm_icon').hide();
+            $('#valid_confirm_icon').show();
+        }
+        return validate;
     });
 
 
