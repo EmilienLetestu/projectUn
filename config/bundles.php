20f13a7cf14a4c49a95424fa5384f313e444039b
emilien_letestu <eletestu@gmail.com>
2017-10-28 14:46:20 +0200

    [mod]|[bug]|[mailer] revert all swiftmailer to original config [unfixed]

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
 ];
