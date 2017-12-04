e8b1715635b98cc69d31ac730b1614bfe43d3a3e
emilien_letestu <eletestu@gmail.com>
2017-12-04 17:46:16 +0100

    [add/mod]|[controller] add search controller / update browse controller with new logic

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -165,8 +165,8 @@ class IndexController extends Controller
      */
     public function browse(Request $request)
     {
-        $view = $this->get('App\Managers\StoryManager')
-            ->fetchForBrowser($request,5)
+        $view = $this->get('App\Builders\BrowserBuilder')
+            ->buildBrowser($request,5)
         ;
 
         return $this->render('pagination.html.twig',[
@@ -175,8 +175,36 @@ class IndexController extends Controller
              'totalPage'  => $view[2],
              'filter'     => $view[3],
              'title'      => $view[4],
+             'country'    => $view[5],
+             'topic'      => $view[6],
+             'patronage'  => $view[7]
             ]
         );
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function search(Request $request)
+    {
+        $view = $this->get('App\Managers\StoryManager')
+            ->processFilterForm($request,5)
+        ;
+
+        if($view[0] === 'all' && $view[1] === 'all' && $view[2] === 'all')
+        {
+            return $this->redirect($this->generateUrl('browse',[
+                'pageNumber'=>1
+                ]));
+        }
+
+        return $this->redirect($this->generateUrl('browse',[
+            'pageNumber'=>1,
+            'country'   => $view[0],
+            'topic'     => $view[1],
+            'patronage' => $view[2]
+        ]));
     }
 }
 
