ab8c46b836c870639935bec5304b8196ba34ff7e
emilien_letestu <eletestu@gmail.com>
2017-12-07 09:38:08 +0100

    [add]|[js] keep user filter selected after redirect

---
+++ public/js/search.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Emilien on 07/12/2017.
+ */
+$(document).ready(function () {
+
+
+    var path = window.location.pathname;
+
+    var split = path.split("/");
+
+
+
+    if(split.length === 8)
+    {
+        var worldArea = split[4];
+        var country   = split[5];
+        var topic     = split[6];
+        var patronage = split[7];
+
+        worldArea !== 'all' ? $('#search_worldArea').val(worldArea) : null;
+        country   !== 'all' ? $('#search_country').val(country) : null;
+        topic     !== 'all' ? $('#search_topic').val(topic) : null;
+        patronage !== 'all' ? $('#search_patronage').val(patronage) : null;
+    }
+
+});
