48a171ae69e23f6b803f6b6d209c652fa28f771c
emilien_letestu <eletestu@gmail.com>
2017-12-21 00:04:08 +0100

    [add/builders] display edit and update story

--- src/Builders/AdminBuilder.php
+++ src/Builders/AdminBuilder.php
@@ -10,9 +10,12 @@ namespace App\Builders;
 
 
 use App\Managers\PatronageManager;
+use App\Managers\StoryManager;
 use App\Managers\TopicManager;
 use App\Services\AddPatronage;
+use App\Services\AddStory;
 use App\Services\AddTopic;
+use App\Services\EditStory;
 use Symfony\Component\HttpFoundation\Request;
 
 class AdminBuilder
@@ -21,6 +24,8 @@ class AdminBuilder
     private $topicManager;
     private $addPatronage;
     private $patronageManager;
+    private $editStory;
+    private $storyManager;
 
 
     /**
@@ -29,18 +34,24 @@ class AdminBuilder
      * @param TopicManager $topicManager
      * @param AddPatronage $addPatronage
      * @param PatronageManager $patronageManager
+     * @param EditStory $editStory
+     * @param StoryManager $storyManager
      */
     public function __construct(
         AddTopic         $addTopic,
         TopicManager     $topicManager,
         AddPatronage     $addPatronage,
-        PatronageManager $patronageManager
+        PatronageManager $patronageManager,
+        EditStory        $editStory,
+        StoryManager     $storyManager
     )
     {
         $this->addTopic         = $addTopic;
         $this->topicManager     = $topicManager;
         $this->addPatronage     = $addPatronage;
         $this->patronageManager = $patronageManager;
+        $this->editStory        = $editStory;
+        $this->storyManager     = $storyManager;
     }
 
     /**
@@ -64,6 +75,18 @@ class AdminBuilder
         return[
             $this->patronageManager->fetchPatronageForAdmin(),
             $this->addPatronage->processPatronage($request)
+        ];
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function buildAdminEditStory(Request $request)
+    {
+        return[
+            $this->storyManager->fetchOneStoryForAdmin($request),
+            $this->editStory->processAndEdit($request)
         ];
     }
 }
