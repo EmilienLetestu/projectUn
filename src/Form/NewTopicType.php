8ed0921bc07e298c1f5e20bbfc1ee73735b9f4fc
emilien_letestu <eletestu@gmail.com>
2017-12-20 17:06:10 +0100

    [add/form] form to create new topic from admin

---
+++ src/Form/NewTopicType.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 20/12/2017
+ * Time: 16:18
+ */
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class NewTopicType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type',TextType::class,[
+                'constraints' => [new NotBlank(),
+                                  new Length(['min' => 3,
+                                              'max' => 20
+                                  ])
+                ],
+                'label'       => 'Enter topic\'s name'
+            ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'App\Entity\Topic']);
+    }
+}
