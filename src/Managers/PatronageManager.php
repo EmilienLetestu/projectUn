ee9e1499821fac2a4485bd187bd21a2ca9308b9f
emilien_letestu <eletestu@gmail.com>
2017-12-20 12:04:16 +0100

    [add/managers] new manager to handle patronage C.R.U.D

---
+++ src/Managers/PatronageManager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 20/12/2017
+ * Time: 11:55
+ */
+
+namespace App\Managers;
+
+
+use App\Entity\Patronage;
+use Doctrine\ORM\EntityManager;
+
+class PatronageManager
+{
+    private $doctrine;
+
+    /**
+     * PatronageManager constructor.
+     * @param EntityManager $doctrine
+     */
+    public function __construct(EntityManager $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @return array
+     */
+    public function fetchPatronageForAdmin()
+    {
+        $repository = $this->doctrine->getRepository(Patronage::class);
+
+        return $repository->findAll();
+    }
+}
