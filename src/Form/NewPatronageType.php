38774c09dd35e0d51c05efd6036fa2492b4066c0
emilien_letestu <eletestu@gmail.com>
2017-12-20 17:37:11 +0100

    [add/form] create new patronage from admin

---
+++ src/Form/NewPatronageType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 20/12/2017
+ * Time: 17:13
+ */
+
+namespace App\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class NewPatronageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+       $builder
+           ->add('organization',TextType::class,[
+               'constraints' => [new NotBlank(),
+                                 new Length(['min' => 3,
+                                             'max' => 30
+                                 ])
+               ],
+               'label' => 'Enter patronage names'
+           ])
+       ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class'=>'App\Entity\Patronage']);
+    }
+}
