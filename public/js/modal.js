7492ceafafe023d0bac4936a6bf956b83b1bba29
emilien_letestu <eletestu@gmail.com>
2018-01-02 12:28:43 +0100

    [mod/js] major modification to "modal" management

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
 
+    var path         = window.location.pathname;
+    var entity       = path.split('/');
+    var hidden       =  $('.modalForm input[type=hidden]');
+
     $('.trigger button').click(function () {
         $('.modalForm').show();
         $('.modalOverlay').show();
@@ -8,13 +12,21 @@ $(document).ready(function () {
     $('.closeModal').click(function () {
         $('.modalForm').hide();
         $('.modalOverlay').hide();
+
+        if(hidden.val() !== " ")
+        {
+            $('.modalForm input[type=hidden]').val(' ');
+            $('.modalForm input[type=text]').val(' ');
+            $('.modalForm label').text('Enter ' + entity[2] + ' name');
+        }
     });
 
     $('.update').click(function (){
-        alert($(this).attr("value"));
-        var id = $(this).attr("value");
-
-        $('#topicForm input[type=hidden]').val(id);
+        var id           = $(this).attr("value");
+        var entityType   = $('#entityType'+id).html();
+        hidden.val(id);
+        $('.modalForm input[type=text]').val(entityType.trim());
+        $('.modalForm label').text('Change '+entity[2]);
     });
 
     if($('.help-block').length){
