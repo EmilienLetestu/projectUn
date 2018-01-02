8b659f518f969ad0dea8e0bb14bf7d919df05203
emilien_letestu <eletestu@gmail.com>
2018-01-03 00:35:03 +0100

    [js] code indent

--- public/js/filterList.js
+++ public/js/filterList.js
@@ -2,10 +2,10 @@
  * Created by Emilien on 02/01/2018.
  */
 $(document).ready(function(){
-    $('input[type=checkbox]').change(function(){
 
+    $('input[type=checkbox]').change(function(){
         var rowToHide = $(this).val();
         $('.'+rowToHide).toggle();
-
     })
+
 });
