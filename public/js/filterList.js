793bb4d70964d8439c9197904c139d610c01b52d
emilien_letestu <eletestu@gmail.com>
2018-01-03 00:32:31 +0100

    [add/js/template] filter stories and users by status

---
+++ public/js/filterList.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Emilien on 02/01/2018.
+ */
+$(document).ready(function(){
+    $('input[type=checkbox]').change(function(){
+
+        var rowToHide = $(this).val();
+        $('.'+rowToHide).toggle();
+
+    })
+});
