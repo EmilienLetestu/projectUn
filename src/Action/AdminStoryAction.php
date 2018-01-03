da2a7db2d8628b7e835c840c4804eaffdd057695
emilien_letestu <eletestu@gmail.com>
2018-01-03 12:32:33 +0100

    [add/adr] action and responder for adminStory

---
+++ src/Action/AdminStoryAction.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 11:41
+ */
+
+namespace App\Action;
+
+
+use App\Managers\StoryManager;
+use App\Responder\AdminStoryResponder;
+use Symfony\Component\HttpFoundation\Request;
+
+final class AdminStoryAction
+{
+    /**
+     * @var StoryManager
+     */
+    private $storyManager;
+
+    /**
+     * AdminStoryAction constructor.
+     * @param StoryManager $storyManager
+     */
+    public function __construct(StoryManager $storyManager)
+    {
+        $this->storyManager =$storyManager;
+    }
+
+    /**
+     * @param AdminStoryResponder $responder
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(AdminStoryResponder $responder, Request $request)
+    {
+        $filter   = $request->attributes->get('filter');
+        $method = 'fetchStoryBy'.ucfirst($filter);
+
+       return $responder(
+            $filter !== null?
+                $storyList = $this->storyManager->$method(
+                    $request->attributes->get('filterId')) :
+                $storyList = $this->storyManager->fetchStoryForAdmin()
+       );
+    }
+}
---
+++ src/Responder/AdminStoryResponder.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 11:41
+ */
+
+namespace App\Responder;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class AdminStoryResponder
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * AdminStoryResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param array $storyList
+     * @return Response
+     */
+    public function __invoke(array $storyList)
+    {
+        return new Response($this->twig->render('admin\adminStory.html.twig',[
+            'storyList' => $storyList[0],
+            'title'     => $storyList[1]
+            ])
+        );
+    }
+}
