eb0b978ddb7bf219c31263a2c05967839f496a61
emilien_letestu <eletestu@gmail.com>
2018-01-03 10:06:59 +0100

    [add/core] action and responder for adminHome

--- src/Action/AdminHomeAction.php
+++ src/Action/AdminHomeAction.php
@@ -9,7 +9,42 @@
 
 namespace App\Action;
 
+use App\Entity\Story;
+use App\Entity\User;
+use App\Responder\AdminHomeResponder;
+use Doctrine\ORM\EntityManager;
+
 final class AdminHomeAction
 {
+    /**
+     * @var EntityManager
+     */
+    private $doctrine;
 
+    /**
+     * AdminHomeAction constructor.
+     * @param EntityManager $doctrine
+     */
+    public function __construct(EntityManager $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @param AdminHomeResponder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(AdminHomeResponder $responder)
+    {
+        $repoUser = $this->doctrine->getRepository(User::class);
+        $repoStory = $this->doctrine->getRepository(Story::class);
+
+        return $responder(
+            count($repoUser->countAll('EDIT')),
+            count($repoUser->countAll('USER')),
+            count($repoUser->countAllUnactivated()),
+            count($repoStory->findAll()),
+            $repoStory->countStories()
+        );
+    }
 }
--- src/Responder/AdminHomeResponder.php
+++ src/Responder/AdminHomeResponder.php
@@ -9,7 +9,43 @@
 
 namespace App\Responder;
 
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
 final class AdminHomeResponder
 {
-    
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * AdminHomeResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param $roleEdit
+     * @param $roleUser
+     * @param $unactivated
+     * @param $totalStory
+     * @param $totalValidated
+     * @return Response
+     */
+    public function __invoke($roleEdit,$roleUser,$unactivated,$totalStory,$totalValidated)
+    {
+        return new Response(
+            $this->twig->render('admin\admin.html.twig',[
+                'roleEdit'       => $roleEdit,
+                'roleUser'       => $roleUser,
+                'unactivated'    => $unactivated,
+                'totalStory'     => $totalStory,
+                'totalValidated' => $totalValidated
+            ])
+        );
+    }
 }
