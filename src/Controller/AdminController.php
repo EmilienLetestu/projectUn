3a49f5a3ecf71bef4ca21925786f3239a593fa53
emilien_letestu <eletestu@gmail.com>
2018-01-03 16:00:57 +0100

    !! delete useless controller !!

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -85,28 +85,4 @@ class AdminController extends Controller
         ]);
     }
 
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function adminEntityManagement(Request $request)
-    {
-        $entity = $request->attributes->get('entity');
-        $action = $request->attributes->get('action');
-
-        $method = $action.ucfirst($entity);
-
-        $entity === 'story' ?
-            $process = $this->get('App\Managers\StoryManager')
-                ->$method($request->attributes->get('id'))
-            :
-            $process = $this->get('App\Managers\UserManager')
-                ->$method($request->attributes->get('id'))
-        ;
-
-        $this->get('session')->getFlashBag()->add('success',$process);
-
-        return $this->redirectToRoute('admin'.ucfirst($entity));
-    }
-
 }
