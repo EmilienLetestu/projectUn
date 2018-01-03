3dd683f1112f57d079c4e645a84a7dac0aa84e6c
emilien_letestu <eletestu@gmail.com>
2018-01-03 10:30:37 +0100

    [add/adr] action and responder for adminUser

---
+++ src/Action/AdminUserAction.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 10:13
+ */
+
+namespace App\Action;
+
+
+use App\Entity\User;
+use App\Responder\AdminUserResponder;
+use Doctrine\ORM\EntityManager;
+
+final class AdminUserAction
+{
+    /**
+     * @var EntityManager
+     */
+    private $doctrine;
+
+    /**
+     * AdminUserAction constructor.
+     * @param EntityManager $doctrine
+     */
+    public function __construct(EntityManager $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @param AdminUserResponder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(AdminUserResponder $responder)
+    {
+        $repository = $this->doctrine->getRepository(User::class);
+
+        return $responder($repository->findAll());
+    }
+}
---
+++ src/Responder/AdminUserResponder.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 10:19
+ */
+
+namespace App\Responder;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class AdminUserResponder
+{
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * AdminUserResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param array $userList
+     * @return Response
+     */
+    public function __invoke(array $userList)
+    {
+        return new Response(
+            $this->twig->render('admin/adminUser.html.twig',[
+                'userList' => $userList
+            ])
+        );
+    }
+}
