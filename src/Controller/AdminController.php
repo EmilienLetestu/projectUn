8d9ba68b096eb17366a03149d090fc63d08f0f54
emilien_letestu <eletestu@gmail.com>
2018-01-03 16:53:41 +0100

    !! delete useless service and controllers !!

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,30 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 class AdminController extends Controller
 {
 
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function adminTopic(Request $request)
-    {
-        $list = $this->get('App\Managers\TopicManager')
-            ->fetchTopicForAdmin();
 
-        $form = $this->get('App\Services\EditTopic')
-            ->processTopic($request);
-
-
-        if($this->get('session')->get('added'))
-        {
-            $this->get('session')->remove('added');
-            return $this->redirectToRoute('adminTopic');
-        }
-
-        return $this->render('admin\adminTopic.html.twig',[
-            'topicList' => $list,
-            'form'      => $form
-        ]);
-    }
 
     /**
      * @param Request $request
@@ -64,25 +41,5 @@ class AdminController extends Controller
         ]);
     }
 
-
-    /**
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function adminEditStory(Request $request)
-    {
-        $story = $this->get('App\Managers\StoryManager')
-            ->fetchOneStoryForAdmin($request->attributes->get('id'))
-        ;
-
-        $form = $this->get('App\Services\EditStory')
-            ->processAndEdit($request, $story)
-        ;
-
-        return $this->render('admin\adminEditStory.html.twig',[
-           'story' => $story,
-           'form'  => $form
-        ]);
-    }
 
 }
