e49938003453a6574e8d2a933de6797600a3367b
emilien_letestu <eletestu@gmail.com>
2018-01-03 11:29:28 +0100

    [add/adr] acttion and responder for adminByStory

---
+++ src/Action/AdminByStoryAction.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 11:05
+ */
+
+namespace App\Action;
+
+
+use App\Entity\Story;
+use App\Responder\AdminByStoryResponder;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+
+final class AdminByStoryAction
+{
+    /**
+     * @var EntityManager
+     */
+    private $doctrine;
+
+    /**
+     * AdminByStory constructor.
+     * @param EntityManager $doctrine
+     */
+    public function __construct(EntityManager $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @param Request $request
+     * @param AdminByStoryResponder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(Request $request, AdminByStoryResponder $responder)
+    {
+        $repository = $this->doctrine->getRepository(Story::class);
+
+        return $responder($repository->findOneBy([
+                'id'=>$request->attributes->get('id')
+            ])
+        );
+    }
+}
