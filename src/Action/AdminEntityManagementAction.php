94762d8cfdb1b76c06dae2142ca894b93b25462d
emilien_letestu <eletestu@gmail.com>
2018-01-03 15:55:14 +0100

    [add/adr] action and responder for adminEntityManagement

---
+++ src/Action/AdminEntityManagementAction.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 15:28
+ */
+
+namespace App\Action;
+
+
+use App\Managers\StoryManager;
+use App\Managers\UserManager;
+use App\Responder\AdminEntityManagementResponder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+final class AdminEntityManagementAction
+{
+
+   private $userManager;
+   private $storyManager;
+   private $session;
+
+   /**
+    * AdminEntityManagementAction constructor.
+    * @param UserManager $userManager
+    * @param StoryManager $storyManager
+    * @param Session $session
+    */
+   public function __construct(
+       UserManager  $userManager,
+       StoryManager $storyManager,
+       Session      $session
+   )
+   {
+       $this->userManager  = $userManager;
+       $this->storyManager = $storyManager;
+       $this->session      = $session;
+   }
+
+   /**
+    * @param Request $request
+    * @param AdminEntityManagementResponder $responder
+    * @return \Symfony\Component\HttpFoundation\RedirectResponse
+    */
+   public function __invoke(Request $request, AdminEntityManagementResponder $responder)
+   {
+       $entity = $request->attributes->get('entity');
+       $action = $request->attributes->get('action');
+
+       $method = $action.ucfirst($entity);
+
+       $entity === 'story' ?
+           $process = $this->storyManager
+               ->$method($request->attributes->get('id'))
+           :
+           $process = $this->userManager
+               ->$method($request->attributes->get('id'))
+       ;
+
+       $this->session->getFlashBag()->add('success',$process);
+
+       return $responder($entity);
+   }
+}
---
+++ src/Responder/AdminEntityManagementResponder.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 15:31
+ */
+
+namespace App\Responder;
+
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+final class AdminEntityManagementResponder
+{
+    /**
+     * @param $entity
+     * @return RedirectResponse
+     */
+    public function __invoke($entity)
+    {
+        return new RedirectResponse("/admin/$entity");
+    }
+}
