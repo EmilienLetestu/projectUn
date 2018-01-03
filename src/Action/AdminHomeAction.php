94c170f41983bab9361ffbeccdf14cd6d5cc2f03
emilien_letestu <eletestu@gmail.com>
2018-01-03 09:30:42 +0100

    [add/core] action and responder for adminHome

---
+++ src/Action/AdminHomeAction.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 09:28
+ */
+
+namespace App\Action;
+
+final class AdminHomeAction
+{
+
+}
---
+++ src/Responder/AdminHomeResponder.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 03/01/2018
+ * Time: 09:27
+ */
+
+namespace App\Responder;
+
+final class AdminHomeResponder
+{
+    
+}
