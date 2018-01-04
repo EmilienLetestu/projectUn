cd94ccd94cb47e1f398c35ce79e674886ef0ce50
emilien_letestu <eletestu@gmail.com>
2018-01-04 16:33:32 +0100

    [add/adr] action and responder for browse

---
+++ src/Action/BrowseAction.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 04/01/2018
+ * Time: 15:46
+ */
+
+namespace App\Action;
+
+
+use App\Entity\Story;
+use App\Form\SearchType;
+use App\Responder\BrowseResponder;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+final class BrowseAction
+{
+   private $formFactory;
+   private $doctrine;
+
+   public function __construct(
+       FormFactoryInterface $formFactory,
+       EntityManager        $doctrine
+   )
+   {
+       $this->formFactory = $formFactory;
+       $this->doctrine    = $doctrine;
+   }
+
+    /**
+     * @param Request $request
+     * @param BrowseResponder $responder
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+   public function __invoke(Request $request,BrowseResponder $responder)
+   {
+       $limit = 6;
+       $filter = $this->formFactory->create(SearchType::class);
+
+       //get current page number from url param
+       $pageNumber = $request->attributes->get('pageNumber');
+       $worldArea  = $request->attributes->get('worldArea');
+       $country    = $request->attributes->get('country');
+       $topic      = $request->attributes->get('topic');
+       $patronage  = $request->attributes->get('patronage');
+
+       //find where to start
+       $firstR = ($pageNumber-1)*$limit;
+
+       $repository = $this->doctrine->getRepository(Story::class);
+
+       if($request->attributes->get('country') === null){
+
+           $storyList = $repository->findAllForBrowser($firstR,$limit);
+
+           //calculate the total number of pages
+           $totalPage = ceil(count($storyList)/$limit);
+
+           if($pageNumber > $totalPage) {
+               throw new NotFoundHttpException('This page doesn\'t exist yet');
+           }
+
+           return $responder(
+               $storyList,
+               $pageNumber,
+               $totalPage,
+               $filter->createView(),
+               'Our climate stories safe  holds '.count($storyList)
+           );
+       }
+
+       //fetch filtered story
+       $storyList = $repository->findAllForBrowser($firstR,$limit,$worldArea,$country,$topic,$patronage);
+
+       return $responder(
+           $storyList,
+           $pageNumber,
+           $totalPage = ceil(count($storyList)/$limit),
+           $filter->createView(),
+           'WE\'VE FOUND '.count($storyList),
+           $country,
+           $topic,
+           $patronage,
+           $worldArea
+       );
+   }
+}
---
+++ src/Responder/BrowseResponder.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 04/01/2018
+ * Time: 15:47
+ */
+
+namespace App\Responder;
+
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class BrowseResponder
+{
+    private $twig;
+
+    /**
+     * BrowseResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param $stories
+     * @param $page
+     * @param $totalPage
+     * @param FormView $form
+     * @param $title
+     * @param $country
+     * @param $topic
+     * @param $patronage
+     * @param $worldArea
+     * @return Response
+     */
+    public function __invoke(
+        $stories,
+        $page,
+        $totalPage,
+        FormView $form,
+        $title,
+        $country = null,
+        $topic = null,
+        $patronage = null,
+        $worldArea = null
+    )
+    {
+        return new Response(
+            $this->twig->render('pagination.html.twig',[
+                'stories'    => $stories,
+                'pageNumber' => $page,
+                'totalPage'  => $totalPage,
+                'filter'     => $form,
+                'title'      => $title,
+                'country'    => $country,
+                'topic'      => $topic,
+                'patronage'  => $patronage,
+                'worldArea'  => $worldArea
+            ])
+        );
+    }
+
+
+}
