a64b565bf50fb28d2a51acf8e26490208fa0d25e
emilien_letestu <eletestu@gmail.com>
2018-01-04 10:40:25 +0100

    [add/adr] action and responder for login

---
+++ src/Action/LoginAction.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 04/01/2018
+ * Time: 10:24
+ */
+
+namespace App\Action;
+
+
+use App\Responder\LoginResponder;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class LoginAction
+{
+    private $authCheck;
+
+    /**
+     * LoginAction constructor.
+     * @param AuthorizationChecker $authCheck
+     */
+    public function  __construct(
+        AuthorizationChecker $authCheck
+    )
+    {
+        $this->authCheck  = $authCheck;
+    }
+
+    /**
+     * @param AuthenticationUtils $authUtils
+     * @param LoginResponder $responder
+     * @return RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(AuthenticationUtils $authUtils, LoginResponder $responder)
+    {
+        if($this->authCheck->isGranted('IS_AUTHENTICATED_REMEMBERED')){
+            return new RedirectResponse('/');
+        }
+
+        return $responder(
+            $authUtils->getLastAuthenticationError(),
+            $authUtils->getLastUsername()
+        );
+    }
+}
---
+++ src/Responder/LoginResponder.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 04/01/2018
+ * Time: 10:25
+ */
+
+namespace App\Responder;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+final class LoginResponder
+{
+    private $twig;
+
+    /**
+     * LoginResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param $userName
+     * @param $error
+     * @return Response
+     */
+    public function __invoke($userName,$error)
+    {
+        return new Response(
+            $this->twig->render('connectionForms.html.twig',[
+                'last_username' =>$userName,
+                'error' => $error
+            ])
+        );
+    }
+}
