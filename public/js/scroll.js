a57b414ece5fc4fb1362d75a79e399828c15066a
emilien_letestu <eletestu@gmail.com>
2018-01-08 23:56:23 +0100

    [add/js] script to handle x-axis scrolling

---
+++ public/js/scroll.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Emilien on 08/01/2018.
+ */
+$(document).ready(function () {
+
+    var scrollHandle = 0,
+        scrollStep = 5,
+        parent = $(".more");
+
+    //Start the scrolling process
+    $(".panner").on("mouseenter", function () {
+        var data = $(this).data('scrollModifier'),
+            direction = parseInt(data, 10);
+
+        $(this).addClass('active');
+
+        startScrolling(direction, scrollStep);
+    });
+
+    //stop scrolling
+    $(".panner").on("mouseleave", function () {
+        stopScrolling();
+        $(this).removeClass('active');
+    });
+
+    //Actual handling of scrolling
+    function startScrolling(modifier, step) {
+        if (scrollHandle === 0) {
+            scrollHandle = setInterval(function () {
+                var newOffset = parent.scrollLeft() + (scrollStep * modifier);
+
+                parent.scrollLeft(newOffset);
+            }, 10);
+        }
+    }
+
+    function stopScrolling() {
+        clearInterval(scrollHandle);
+        scrollHandle = 0;
+    }
+});
