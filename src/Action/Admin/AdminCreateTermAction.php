ea9dc12040c7db28c3330fc3e28440c78e6af126
emilien_letestu <eletestu@gmail.com>
2018-01-11 22:23:07 +0100

    [add/adr]  action and responder for admin create term

---
+++ src/Action/Admin/AdminCreateTermAction.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 11/01/2018
+ * Time: 21:33
+ */
+
+namespace App\Action\Admin;
+
+
+use App\Entity\Term;
+use App\Form\TermType;
+use App\Responder\Admin\AdminCreateTermResponder;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+
+class AdminCreateTermAction
+{
+    private $formFactory;
+    private $doctrine;
+    private $urlGenerator;
+    private $session;
+
+    /**
+     * AdminCreateTermAction constructor.
+     * @param FormFactoryInterface $formFactory
+     * @param EntityManagerInterface $doctrine
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param SessionInterface $session
+     */
+    public function __construct(
+        FormFactoryInterface   $formFactory,
+        EntityManagerInterface $doctrine,
+        UrlGeneratorInterface  $urlGenerator,
+        SessionInterface       $session
+    )
+    {
+        $this->formFactory  = $formFactory;
+        $this->doctrine     = $doctrine;
+        $this->urlGenerator = $urlGenerator;
+        $this->session      = $session;
+    }
+
+
+    /**
+     * @param Request $request
+     * @param AdminCreateTermResponder $responder
+     * @return string|\Symfony\Component\HttpFoundation\Response
+     */
+    public function __invoke(Request $request, AdminCreateTermResponder $responder)
+    {
+        $term = new Term();
+        $form = $this->formFactory
+                     ->create(TermType::class, $term)
+                     ->handleRequest($request)
+        ;
+        dump($form);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $this->doctrine->getRepository(Term::class);
+            $this->doctrine->persist($term);
+            $this->doctrine->flush();
+
+            return $this->urlGenerator
+                         ->generate('adminCreateTerm')
+            ;
+        }
+
+        return $responder($form->createView());
+    }
+}
---
+++ src/Responder/Admin/AdminCreateTermResponder.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 11/01/2018
+ * Time: 21:38
+ */
+
+namespace App\Responder\Admin;
+
+
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class AdminCreateTermResponder
+{
+    private $twig;
+
+    /**
+     * AdminCreateTermResponder constructor.
+     * @param Environment $twig
+     */
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param FormView $form
+     * @return Response
+     */
+    public function __invoke(FormView $form)
+    {
+       return new Response(
+           $this->twig->render(['admin\adminCreateTerm.html.twig',
+               'form' => $form
+           ])
+       );
+    }
+}
