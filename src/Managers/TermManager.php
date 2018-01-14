d91543e55c1ddc48f0cd0eecedd50aa9b459c3a3
emilien_letestu <eletestu@gmail.com>
2018-01-14 13:01:33 +0100

    [add/manager] create "TermManager"

---
+++ src/Managers/TermManager.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 14/01/2018
+ * Time: 12:42
+ */
+
+namespace App\Managers;
+
+
+use App\Entity\Term;
+use Doctrine\ORM\EntityManager;
+
+class TermManager
+{
+    private $doctrine;
+
+    /**
+     * TermManager constructor.
+     * @param EntityManager $doctrine
+     */
+    public function __construct(EntityManager $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
+     * @param $id
+     * @return string
+     */
+    public function deleteTerm($id)
+    {
+        $repository = $this->doctrine->getRepository(Term::class);
+        $term = $repository->find($id);
+
+        $this->doctrine->remove($term);
+        $this->doctrine->flush();
+
+        return 'Legal notice article has been deleted';
+    }
+}
