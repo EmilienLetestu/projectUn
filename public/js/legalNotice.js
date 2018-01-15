ae64f294a0238ce8f444a0c74b0418d6a81d0b4a
emilien_letestu <eletestu@gmail.com>
2018-01-15 12:21:29 +0100

    [add/template/js] legal notice dedicated template and js script

---
+++ public/js/legalNotice.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Emilien on 15/01/2018.
+ */
+$(document).ready(function () {
+
+    $(".termBody:first").show();
+    $(".toggleIcon:first").addClass("fa-caret-down").removeClass("fa-caret-right");
+
+    $(".toggleIcon").click(function () {
+        var id = this.id.split('-');
+        var toToggle = "#term-" + id[1];
+        if($(this).hasClass("fa-caret-right")){
+           $(this).addClass("fa-caret-down").removeClass("fa-caret-right");
+        }
+        else{
+            $(this).addClass("fa-caret-right").removeClass("fa-caret-down");
+        }
+        $("i", this).toggleClass("fa fa-caret-right fa fa-caret-down");
+        $(toToggle).toggle();
+    })
+});
