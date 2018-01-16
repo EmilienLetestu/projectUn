94f74cfb865ac9bd0aea395bba8ba5cb43333757
emilien_letestu <eletestu@gmail.com>
2018-01-16 22:12:21 +0100

    [mod/js/template] reset filter button

--- public/js/search.js
+++ public/js/search.js
@@ -5,10 +5,7 @@ $(document).ready(function () {
 
 
     var path = window.location.pathname;
-
     var split = path.split("/");
-
-
 
     if(split.length === 8)
     {
@@ -21,6 +18,10 @@ $(document).ready(function () {
         country   !== 'all' ? $('#search_country').val(country) : null;
         topic     !== 'all' ? $('#search_topic').val(topic) : null;
         patronage !== 'all' ? $('#search_patronage').val(patronage) : null;
-    }
+        $("#resetBtn").show();
 
+        if($(window).width() < 768) {
+            $("#browseStories").css('padding-top', '20%');
+        }
+    }
 });
