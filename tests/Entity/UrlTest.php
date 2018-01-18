89d6728788f62155ab63d308e53a6b1a59312738
emilien_letestu <eletestu@gmail.com>
2018-01-18 16:15:47 +0100

    [test/entity] url entiy test

---
+++ tests/Entity/UrlTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Emilien
+ * Date: 18/01/2018
+ * Time: 16:04
+ */
+
+namespace tests\Entity;
+
+
+use App\Entity\Url;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class UrlTest extends TestCase
+{
+    public function urlTest()
+    {
+        $url = new Url();
+
+        $href    = 'https://openclassrooms.com';
+        $host    = 'openclassrooms.com';
+
+        $url->setHref($href);
+        $url->setAlt($href);
+
+        //test
+        static::assertEquals($href, $url->getHref());
+        static::assertEquals($host, $url->getAlt());
+
+    }
+}
