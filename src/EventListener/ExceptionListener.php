3da12c56addf0cae5008de4ff2eeecab306bd9c0
emilien_letestu <eletestu@gmail.com>
2018-01-22 00:46:22 +0100

    [mod/listener] catch 403 + use switch statement

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -11,6 +11,7 @@ namespace App\EventListener;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 class ExceptionListener
@@ -34,24 +35,28 @@ class ExceptionListener
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $exception = $event->getException();
+
         $response = new Response();
 
-        if($exception instanceof HttpExceptionInterface && $exception->getStatusCode() === 404)
-        {
-            $template = $this->twig->render('error.html.twig',[
-               'message'=>'This page doesn\'t exist yet !',
-            ]);
-            $response->setContent($template);
+        switch (true){
+            case($exception instanceof HttpExceptionInterface && $exception->getStatusCode() === 404):
+                $template = $this->twig->render('error.html.twig', [
+                        'message' => 'This page doesn\'t exist yet !']
+                );
+                $response->setContent($template);
+                break;
+            case($exception instanceof HttpExceptionInterface && $exception->getStatusCode() === 403):
+                $template = $this->twig->render('error.html.twig', [
+                        'message' => 'Sorry but you\'re not allowed to access this page']
+                );
+                $response->setContent($template);
+                break;
+            default:$template = $this->twig->render('error.html.twig',[
+                    'message' => 'We are experiencing technical issues, please try again later on.']
+            );
+                $response->setContent($template);
         }
-
-       else {
-            $template = $this->twig->render('error.html.twig',[
-                'message' => 'We are experiencing technical issues, please try again later on.',
-            ]);
-            $response->setContent($template);
-        }
-
-        $event->setResponse($response);
+        return $event->setResponse($response);
     }
 }
 
